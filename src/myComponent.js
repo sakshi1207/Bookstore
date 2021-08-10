47eb51c3f4cea4a1ffee47b40e148401bf8356af
sakshi <sakshianandk@gmail.com>
2021-08-10 10:55:57 +0530

    My component

---
+++ src/myComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "react-dom";
+
+function MyComponent(){
+
+    return(
+        <React.Fragment>
+           
+            <div>
+                <h2>Sakshi</h2>
+                <h2>Age:20</h2>
+                <img src="user.jpg" alt="My Image" ></img>
+            </div>
+        </React.Fragment>
+    )
+}
+
+
+export default MyComponent
