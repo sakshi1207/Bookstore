bf3020cdc7957172306e98ae176daf079d622b1f
sakshi <sakshianandk@gmail.com>
2021-08-10 14:56:16 +0530

    more with css

--- src/Bookstore.js
+++ src/Bookstore.js
@@ -29,6 +29,6 @@ const Book = () => {
 const Img =() => <img src="https://images.unsplash.com/photo-1589998059171-988d887df646?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80" alt="" width="200" height="200"></img>
 export default Bookstore
 
-const Title = () => <h1>Great Stories for Children</h1>
+const Title = () => <h1 style={{color:'black',fontSize:'1.25rem'}}>Great Stories for Children</h1>
 
-const Author = () => <h2>Ruskin Bond</h2>
+const Author = () => <h2 style={{color:'grey',fontSize:'1rem',marginTop:'0.75rem'}}>Ruskin Bond</h2>
